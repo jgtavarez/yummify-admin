d65561d26907f9517e6051e31b587e2df95c1c12
Juan Gabriel Tavárez López <jtavarez@intellisys.com.do>
2022-03-27 22:40:02 -0400

    build: added heroku deploy config

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('./dist/yummify-admin'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/yummify-admin/'}),
+);
+
+app.listen(process.env.PORT || 8080);
